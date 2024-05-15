5a4fddd214803fa22888ab4a8c904ee806144b3d
Miguel Leon <Miguel.Leon@unh.edu>
2024-05-15 16:46:44 -0600

    Create loadplantnet.R

---
+++ loadplantnet.R
@@ -0,0 +1,9 @@
+install.packages("devtools")
+install.packages("httr")
+install.packages("jsonlite")
+install.packages("magrittr")
+install.packages("dplyr")
+library(devtools)
+
+devtools::install_github(repo = 'BiologicalRecordsCentre/plantnet')
+library(plantnet)
